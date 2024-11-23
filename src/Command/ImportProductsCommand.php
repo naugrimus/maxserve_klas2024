3095062f087baef71dcd0b202d788b544378bafb
Niels Veenstra <naugrimus@gmail.com>
2024-11-23 23:00:58 +0100

    code cleanup

--- src/Command/ImportProductsCommand.php
+++ src/Command/ImportProductsCommand.php
@@ -2,22 +2,13 @@
 
 namespace App\Command;
 
-use App\Entity\Product;
-use App\Entity\ProductBrand;
-use App\Entity\ProductCategory;
-use App\Entity\ProductImage;
 use App\Factories\ProductEntityFactory;
-use App\Services\ImageHandler\imageHandlerInterface;
-use App\Services\ProductApi\DataFetcherInterface;
 use App\Services\ProductImporterInterface;
-use Doctrine\DBAL\Driver\Exception;
-use stdClass;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-
 
 #[AsCommand(
     name: 'app:import-products',
@@ -32,7 +23,6 @@ class ImportProductsCommand extends Command
     protected bool $useLocalImages;
 
     protected string $url;
-    protected imageHandlerInterface $imageHandler;
 
     protected ProductImporterInterface $productImporter;
 
@@ -47,7 +37,6 @@ class ImportProductsCommand extends Command
         $this->url = $url;
         $this->productImporter = $productImporter;
         $this->style = $style;
-
 
         parent::__construct();
     }
